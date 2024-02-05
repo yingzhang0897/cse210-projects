32d44c29bdaaa84eb9cb56b3eebe811aa9493535
“yingzhang0897” <yinganticpc@gmail.com>
2024-02-04 21:59:10 -0800

    finished learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("yellow", 3));
+        shapes.Add(new Rectangle("green", 3,4));
+        shapes.Add(new Circle("blue", 5));
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,18 @@
+public abstract class Shape
+{
+    private string _color;
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+    public abstract double GetArea();
+
+}
